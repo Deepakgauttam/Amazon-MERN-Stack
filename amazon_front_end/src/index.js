ad0d09fe6d77ec3a36fce83ad986ce4928ccb422
Deepakgauttam <105534881+Deepakgauttam@users.noreply.github.com>
2023-01-08 06:25:29 +0530

    Language Translation

--- amazon_front_end/src/index.js
+++ amazon_front_end/src/index.js
@@ -2,11 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import common_bd from "./translations/bd/common.json";
+import common_en from "./translations/en/common.json";
+import common_pk from "./translations/pk/common.json";
+import common_in from "./translations/in/common.json";
 
+
+i18next.init({
+  whitelist: ["bd", "en", "pk", "in"],
+  interpolation: { escapeValue: false }, // React already does escaping
+  lng: "en", // language to use
+  resources: {
+    en: {
+      common: common_en, // 'common' is our custom namespace
+    },
+    bd: {
+      common: common_bd,
+    },
+    pk: {
+      common: common_pk,
+    },
+    in: {
+      common: common_in,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>
 );
